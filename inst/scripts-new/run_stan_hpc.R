f7c246245e74e9bf8a53b119783ddb7d1110e6ef
Mélodie MONOD <monod.melodie@gmail.com>
2021-11-01 11:08:42 +0000

    opt for stan model

--- inst/scripts-new/run_stan_hpc.R
+++ inst/scripts-new/run_stan_hpc.R
@@ -111,7 +111,7 @@ cat("The reference date is", as.character(ref_date), "\n")
 cat("\n Prepare stan data \n")
 stan_data = prepare_stan_data(deathByAge, loc_name, ref_date); data <- tmp
 
-if(grepl('211014|211019|211020|211025|211026|211027|211029|211030', stan_model)){
+if(grepl('211014|211019|211020|211025|211026|211027|211029|211030|211031', stan_model)){
   cat("\n Using 2D splines \n")
   stan_data = add_2D_splines_stan_data(stan_data, spline_degree = 3, n_knots_rows = 12, n_knots_columns = 10)
 }
@@ -119,7 +119,7 @@ if(grepl('211015', stan_model)){
   cat("\n Adding adjacency matrix on 2D splines parameters \n")
   stan_data = add_adjacency_matrix_stan_data(stan_data, n = stan_data$num_basis_row, m = stan_data$num_basis_column)
 }
-if(grepl('211014b|211019|211020|211025|211026|211027|211029|211030', stan_model)){
+if(grepl('211014b|211019|211020|211025|211026|211027|211029|211030|211031', stan_model)){
   cat("\n With vaccine effects \n")
   resurgence_dates <- find_resurgence_dates(JHUData, deathByAge, Code)
   stan_data = add_resurgence_period(stan_data, df_week, resurgence_dates)
