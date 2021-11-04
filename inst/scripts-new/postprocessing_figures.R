de9b0fdc9006cd9869be7cf59c4ede4e0dbf8a4f
Mélodie MONOD <monod.melodie@gmail.com>
2021-11-04 08:38:19 +0000

    minor change

--- inst/scripts-new/postprocessing_figures.R
+++ inst/scripts-new/postprocessing_figures.R
@@ -152,7 +152,7 @@ if(!is.null(stan_data$prop_vac)){
   make_var_by_age_table(fit_cum, df_week, df_age_vaccination2, 'phi_reduced_vac', outdir.table)
 
   E_pdeaths = make_var_cum_by_age_table(fit_cum, df_week, df_age_vaccination2, 'E_pdeaths', outdir.table)
-  E_pdeaths_counterfactual = make_var_cum_by_age_table(fit_cum, df_week2, df_age_vaccination2, 'E_pdeaths_counterfactual', outdir.table)
+  E_pdeaths_counterfactual = make_var_cum_by_age_table_counterfactual(fit_cum, df_week, df_week2, resurgence_dates, df_age_vaccination2, c('E_pdeaths', 'E_pdeaths_counterfactual'), outdir.table)
   diff_E_pdeaths_counterfactual = make_var_by_age_table(fit_cum, df_week2, df_age_vaccination2, 'diff_E_pdeaths_counterfactual', outdir.table)
   perc_E_pdeaths_counterfactual = make_var_by_age_table(fit_cum, df_week2, df_age_vaccination2, 'perc_E_pdeaths_counterfactual', outdir.table)
   plot_vaccine_effects_counterfactual(E_pdeaths_counterfactual, E_pdeaths, diff_E_pdeaths_counterfactual,perc_E_pdeaths_counterfactual, resurgence_dates, outdir.fig)
