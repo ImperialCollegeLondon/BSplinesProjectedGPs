2e84471df7d6687c93f640dcf147a7c059a1bd9c
Mélodie MONOD <monod.melodie@gmail.com>
2021-11-07 11:19:45 +0000

    minor change proc file

--- inst/run-model.sh
+++ inst/run-model.sh
@@ -5,30 +5,28 @@ STAN_MODEL="211031b1"
 CWD="/Users/melodiemonod/git/covid19Vaccination/inst/results-try/"
 INDIR="/Users/melodiemonod/git/covid19Vaccination/inst/"
 STATES='CA,FL,NY,TX'
-#STATES='CA'
+
+mdkdir $CWD
   
 cat > $CWD/bash_$STAN_MODEL-$JOBID.pbs <<EOF
   
 #!/bin/sh
   
-PWD=\$(pwd)
 CWD=$CWD
 INDIR=$INDIR
 STAN_MODEL=$STAN_MODEL
 JOBID=$JOBID
   
 # main directory
-mkdir \$PWD/\$STAN_MODEL-\$JOBID
+mkdir \$CWD/\$STAN_MODEL-\$JOBID
   
 # directories for fits, figures and tables
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/fits
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/data
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/figure
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/table
+mkdir \$CWD/\$STAN_MODEL-\$JOBID/fits
+mkdir \$CWD/\$STAN_MODEL-\$JOBID/data
+mkdir \$CWD/\$STAN_MODEL-\$JOBID/figure
+mkdir \$CWD/\$STAN_MODEL-\$JOBID/table
   
-Rscript \$INDIR/scripts/run_stan_hpc.R -indir \$INDIR -outdir \$PWD -states $STATES -stan_model \$STAN_MODEL -JOBID \$JOBID
-  
-cp -R --no-preserve=mode,ownership \$PWD/* \$CWD
+Rscript \$INDIR/scripts/run_stan_hpc.R -indir \$INDIR -outdir \$CWD -states $STATES -stan_model \$STAN_MODEL -JOBID \$JOBID
   
 cd \$CWD
 
@@ -39,23 +37,22 @@ cat > $CWD/bash_$STAN_MODEL-$JOBID-postprocessing.pbs <<EOF
 #!/bin/sh
   
 CWD=$CWD
-PWD=$CWD
 INDIR=$INDIR
 STAN_MODEL=$STAN_MODEL
 JOBID=$JOBID
   
 # main directory
-mkdir \$PWD/\$STAN_MODEL-\$JOBID
+mkdir \$CWD/\$STAN_MODEL-\$JOBID
   
 # directories for fits, figures and tables
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/fits
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/data
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/figure
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/table
+mkdir \$CWD/\$STAN_MODEL-\$JOBID/fits
+mkdir \$CWD/\$STAN_MODEL-\$JOBID/data
+mkdir \$CWD/\$STAN_MODEL-\$JOBID/figure
+mkdir \$CWD/\$STAN_MODEL-\$JOBID/table
   
-Rscript \$INDIR/scripts/postprocessing_assess_mixing.R -indir \$INDIR -outdir \$PWD -states $STATES -stan_model \$STAN_MODEL -JOBID \$JOBID
-Rscript \$INDIR/scripts/postprocessing_figures.R -indir \$INDIR -outdir \$PWD -states $STATES -stan_model \$STAN_MODEL -JOBID \$JOBID
-Rscript \$INDIR/scripts/postprocessing_union.R -indir \$INDIR -outdir \$PWD -states $STATES -stan_model \$STAN_MODEL -JOBID \$JOBID
+Rscript \$INDIR/scripts/postprocessing_assess_mixing.R -indir \$INDIR -outdir \$CWD -states $STATES -stan_model \$STAN_MODEL -JOBID \$JOBID
+Rscript \$INDIR/scripts/postprocessing_figures.R -indir \$INDIR -outdir \$CWD -states $STATES -stan_model \$STAN_MODEL -JOBID \$JOBID
+Rscript \$INDIR/scripts/postprocessing_union.R -indir \$INDIR -outdir \$CWD -states $STATES -stan_model \$STAN_MODEL -JOBID \$JOBID
   
 EOF
 
