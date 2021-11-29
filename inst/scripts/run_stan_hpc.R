001a6a93b7ce68d3e300e0efb744bd202ad5820e
Mélodie MONOD <monod.melodie@gmail.com>
2021-11-29 10:08:12 +0000

    new stan model

--- inst/scripts/run_stan_hpc.R
+++ inst/scripts/run_stan_hpc.R
@@ -10,7 +10,7 @@ library(ggpubr)
 indir ="~/git/BSplinesProjectedGPs/inst" # path to the repo
 outdir = file.path('~/Downloads/', "results")
 states = strsplit('CA,FL,NY,TX,PA,IL,OH,GA,NC,MI',',')[[1]]
-stan_model = "211127a"
+stan_model = "211130a"
 JOBID = 3541
 
 if(0)
@@ -115,7 +115,7 @@ cat("The reference date is", as.character(ref_date), "\n")
 cat("\n Prepare stan data \n")
 stan_data = prepare_stan_data(deathByAge, loc_name, ref_date); data <- tmp
 
-if(grepl('211125a|211125c|211125d|211127a', stan_model)){
+if(grepl('211125a|211125c|211125d|211127a|211129a|211130a', stan_model)){
   cat("\n Using 2D splines \n")
   stan_data = add_2D_splines_stan_data(stan_data, spline_degree = 3, n_knots_rows = 12, n_knots_columns = 10)
 }
