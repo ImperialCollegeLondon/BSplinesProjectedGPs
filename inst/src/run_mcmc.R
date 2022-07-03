b5c8394bc5d7f625ec68ec2464fb3024fad78835
Mélodie MONOD <monod.melodie@gmail.com>
2022-07-03 19:25:40 +0100

    bugfix

--- inst/src/run_mcmc.R
+++ inst/src/run_mcmc.R
@@ -11,15 +11,14 @@ library("doParallel")
 library("prodlim")
 library("tidyverse")
 library("igraph")
-library("ramcmc")
-library("lognorm")
-library('metRology')
-library(abind)
-library(compiler)
+# library("ramcmc")
+# library("lognorm")
+# library('metRology')
 
-indir <- "~/git/BSplinesProjectedGPs"
+indir <- "~/git/BSplinesProjectedGPs/inst"
 stan_model <- '220209a'
 JOBID <- '1081'
+states = strsplit('FL,NY,TX,PA,IL,OH,GA,NC,MI',',')[[1]]
 
 if(1){
   outdir <- "~/git/BSplinesProjectedGPs/src/results"
@@ -38,41 +37,44 @@ if(length(args_line) > 0)
 {
   stopifnot(args_line[[1]]=='-indir')
   stopifnot(args_line[[3]]=='-outdir')
-  stopifnot(args_line[[5]]=='-datadir')
+  stopifnot(args_line[[5]]=='-states')
   stopifnot(args_line[[7]]=='-stan_model')
   stopifnot(args_line[[9]]=='-JOBID')
   
-  indir <- args_line[[2]]  
+  indir <- args_line[[2]]
   outdir <- args_line[[4]]
-  datadir <- args_line[[6]]
+  states <-  strsplit((args_line[[6]]),',')[[1]]
   stan_model <- args_line[[8]]
   JOBID <- as.numeric(args_line[[10]])
-
 } 
 
 # load functions
-source(file.path(indir, 'inst', 'src', "mcmc.R"))
-source(file.path(indir, 'inst', 'src', "mcmc-utils.R"))
+source(file.path(indir, 'src', "mcmc.R"))
+source(file.path(indir, 'src', "mcmc-utils.R"))
 
 # set directories
 run_tag = paste0(stan_model, "-", JOBID)
-outdir.table = file.path(outdir, run_tag, "table", run_tag)
+datadir.table = file.path(outdir, run_tag, "table", run_tag)
+datadir.fit.post = file.path(outdir, run_tag, "fits")
+datadir.data = file.path(outdir, run_tag, "data")
 
 # code
-locations = readRDS( file.path(outdir.fit.post, paste0("location_", run_tag,".rds")) )
+locations = readRDS( file.path(outdir, paste0("location_", run_tag,".rds")) )
 loc_name = locations[code %in% states,]$loc_label
 Code = locations[code %in% states, ]$code
 df_state = data.table(loc_label = loc_name, code = Code, state_index = 1:length(Code))
 
 # load image 
-load(file.path(outdir.data, paste0("stanin_",run_tag,".RData")))
+load(file.path(datadir.data, paste0("stanin_",run_tag,".RData")))
 
 # resurgence deats
-r_pdeaths  = vector(mode = 'list', length = length(locs))
-for(i in seq_along(locs)){
-  r_pdeaths[[i]] = readRDS(paste0(outdir.table, '-r_pdeathsPosteriorSamples_', locs[i], '.rds'))
+r_pdeaths  = vector(mode = 'list', length = length(Code))
+for(i in seq_along(Code)){
+  r_pdeaths[[i]] = readRDS(paste0(datadir.table, '-r_pdeathsPosteriorSamples_', Code[i], '.rds'))
 }
 r_pdeaths = do.call('rbind', r_pdeaths)
+set(r_pdeaths, NULL, 'state_index', NULL)
+r_pdeaths <- merge(r_pdeaths, df_state, by = c('code', 'loc_label'))
 
 #format r_pdeaths
 r_pdeaths <- r_pdeaths[order(iterations, state_index, age_index, week_index)]
@@ -87,17 +89,25 @@ for( i in sort(unique(r_pdeaths$iterations))){
 r_pdeaths <- copy(r_pdeaths_list)
 
 # iterations and warmup
-iter = max(r_pdeaths$iterations)
+iter = length(r_pdeaths)
 warmup = 5
 
 # adapt 
 adapt = T
 
+# add data for all countries
+stan_data = prepare_stan_data(deathByAge, loc_name, ref_date); data <- tmp
+stan_data = add_JHU_data(stan_data, df_week, Code)
+stan_data = add_vaccine_age_strata(stan_data, df_age_vaccination)
+resurgence_dates <- find_resurgence_dates(JHUData, deathByAge, locations$code)[code %in% Code]
+stan_data = add_resurgence_period(stan_data, df_week, resurgence_dates)
+stan_data = add_vaccine_prop(stan_data, df_week, Code, vaccine_data, resurgence_dates)
+
 # sampling
-MetrHastrw_outputs = MetrHastrw(iter, warmup, r_pdeaths_list, stan_data, adapt)
+MetrHastrw_outputs = MetrHastrw(iter, warmup, r_pdeaths, stan_data, adapt)
 
 # save
-file = paste0(outdir.table, '-vaccination_PosteriorSamples.rds')
+file = paste0(datadir.table, '-vaccination_PosteriorSamples.rds')
 cat('\n Save file ', file, ' \n')
 saveRDS(MetrHastrw_outputs, file = file)
 
